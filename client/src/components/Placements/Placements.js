ea488d509c68e9b5e47c7fdcbc3caae0fea0b40f
Gourav Kargwal <gouravkargwalverma@gmail.com>
2022-01-22 13:49:27 +0530

    Minor bugs

--- client/src/components/Placements/Placements.js
+++ client/src/components/Placements/Placements.js
@@ -2,6 +2,17 @@ import React from "react";
 import PlacementList from "./PlacementList";
 import SearchInput from "./SearchInput";
 import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+
+const PlacementSubSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+    height: 15vh;
+    justify-content: space-evenly;
+  }
+`;
 
 const Placements = () => {
   const props = useSpring({
@@ -11,13 +22,13 @@ const Placements = () => {
   });
   return (
     <animated.div className="placement-section" style={props}>
-      <div className="placement-subsection">
+      <PlacementSubSection>
         <SearchInput />
         <div>
           <button className="add-btn">Add Exp</button>
           <button className="guide-btn">Guidelines</button>
         </div>
-      </div>
+      </PlacementSubSection>
       <div>
         <PlacementList />
       </div>
